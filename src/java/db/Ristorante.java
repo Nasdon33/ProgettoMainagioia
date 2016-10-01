7b05f83a95fde5621ab5025a5b3768319ce126c6
birrando <brando.caserotti@yahoo.it>
2016-10-01 10:05:01 +0200

    Update Ristorante.java

--- src/java/db/Ristorante.java
+++ src/java/db/Ristorante.java
@@ -43,12 +43,46 @@ public class Utente implements Serializable {
         this.offLink = offLink;
     }
 
-     public void setNome(String nome) {
-        this.nome = nome;
+     public void setCucine(String cucine) {
+        this.cucine = cucine;
     }
     
     
-    public String getDescrizione() {
-        return cognome;
+    public String getCucine() {
+        return cucine;
     }
+    
+    
+    public void setCoordinate(String coordinate){
+        this.coordinate=coordinate;
+    }
+    
+    public String getCoordinate(){
+        return coordinate;
+    }
+    
+    public void setOrari(String orari){
+        this.orari=orari;
+    }
+    
+    public String getOrari(){
+        return orari;
+    }
+    
+    public void setFasciaPrezzo(String fasciaPrezzo){
+        this.fasciaPrezzo=fasciaPrezzo;
+    }
+    
+    public String getFasciaPrezzo(){
+        return fasciaPrezzo;
+    }
+    
+    public void setVal(int val){
+        this. val=val;
+    }
+    
+    public int getVal(){
+        return val;
+    }
+    
 }
