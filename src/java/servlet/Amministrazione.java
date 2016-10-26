32a949c82848d22ad9c2b0d45bc4986158599d3f
Nasdon33 <carlo.1994@live.it>
2016-10-26 14:03:08 +0200

    Prova

--- src/java/servlet/Amministrazione.java
+++ src/java/servlet/Amministrazione.java
@@ -3,7 +3,7 @@ package servlet;
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * and open the template in the editor
  */
 
 /**
