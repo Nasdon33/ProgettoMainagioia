813f81e7d6accc5111987e7d33b627ea4381f92b
Nasdon33 <carlo.1994@live.it>
2017-01-16 18:02:47 +0100

    Modifica a Coordinate.Java

--- src/java/servlet/Coordinate.java
+++ src/java/servlet/Coordinate.java
@@ -6,9 +6,15 @@
 package servlet;
 
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
+import java.nio.charset.Charset;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.xpath.XPath;
@@ -16,6 +22,8 @@ import javax.xml.xpath.XPathExpression;
 import javax.xml.xpath.XPathFactory;
 import javax.xml.xpath.XPathConstants;
 import org.w3c.dom.Document;
+import org.json.JSONObject;
+import org.json.JSONException;
 
 /**
  *
@@ -57,5 +65,40 @@ public class Coordinate
     return null;
   }
   
-  //Implementare anche una funzione che restituisca l'indirizzo con le coordinate in input
+  
+  private static String readAll(Reader rd) throws IOException {
+    StringBuilder sb = new StringBuilder();
+    int cp;
+    while ((cp = rd.read()) != -1) {
+      sb.append((char) cp);
+    }
+    return sb.toString();
+  }
+
+  public static JSONObject readJsonFromUrl(String url) throws IOException, JSONException {
+    InputStream is = new URL(url).openStream();
+    try {
+      BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
+      String jsonText = readAll(rd);
+      JSONObject json = new JSONObject(jsonText);
+      return json;
+    } finally {
+      is.close();
+    }
+  }
+  
+  
+  public  static String getAddress(String[] coordinate) throws JSONException, IOException{
+      
+        String webrequest= "http://maps.googleapis.com/maps/api/geocode/json?latlng="+coordinate[0]+","+coordinate[1]+"&sensor=false";
+        webrequest = "http://maps.googleapis.com/maps/api/geocode/json?latlng=23.45,56.87339&sensor=false";
+        JSONObject json = readJsonFromUrl(webrequest);
+        String address = (String) (json.get("formatted_address"));
+        System.out.println(address);
+        return address;
+
+
+
+  }
+  
 }
