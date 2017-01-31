ef2262b41f1487ba80e19977157eb4818ab98d9a
Nasdon33 <carlo.1994@live.it>
2017-01-31 16:46:03 +0100

    Corretto send password

--- web/js/sendPass.js
+++ web/js/sendPass.js
@@ -2,10 +2,10 @@ function sendPassword(){
    $.ajax({
        url: "sendPass",
        type: "POST",
-       data: {email: $("#login input[name=email]").val()},
+       data: {email: $("input[name=email]").val()},
        success: function(esiste){
            if(esiste==="true"){
-               alert("Email inviata all'indirizzo: "+$("#login input[name=email]").val());
+               alert("Email inviata all'indirizzo: "+$("input[name=email]").val());
            }
            else if (esiste==="false"){
                alert("Email non valida");
