db1215ba4c7e51b8cde9f3756072c2eed993afb4
Nasdon33 <carlo.1994@live.it>
2017-02-06 11:21:18 +0100

    Sistemato redirect recensione

--- src/java/servlet/Recensione.java
+++ src/java/servlet/Recensione.java
@@ -100,10 +100,12 @@ public class Recensione extends HttpServlet {
                 String sql2= "INSERT INTO mainagioia.Reviews(ID,global_value, food, service, value_for_money, atmosphere, name,description,date_creation,id_restaurant,id_creator,id_photo) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, null)";
                 manager.setData(sql2, id, val, foo, ser, vfm, atm, nam, des, dat, idr, idc);
         
-                response.sendRedirect("index.jsp");
+                response.sendRedirect(request.getHeader("referer"));
         } catch (SQLException ex) {
+            response.sendRedirect(request.getHeader("referer"));
             Logger.getLogger(Registrazione.class.getName()).log(Level.SEVERE, null, ex);
         } catch (Exception ex) {
+            response.sendRedirect(request.getHeader("referer"));
             Logger.getLogger(Registrazione.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
